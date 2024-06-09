8ced1abb95be0783854982956916ff6fc7a71813
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2024-06-09 21:21:21 +0100

    Update climate.py

--- components/tclac/climate.py
+++ components/tclac/climate.py
@@ -25,7 +25,6 @@ from esphome.components.climate import (
 )
 
 AUTO_LOAD = ["climate"]
-CODEOWNERS = ["@I-am-nightingale", "@xaxexa", "@junkfix"]
 DEPENDENCIES = ["climate", "uart"]
 
 TCLAC_MIN_TEMPERATURE = 16.0
@@ -33,8 +32,6 @@ TCLAC_MAX_TEMPERATURE = 31.0
 TCLAC_TARGET_TEMPERATURE_STEP = 1.0
 TCLAC_CURRENT_TEMPERATURE_STEP = 1.0
 
-CONF_RX_LED = "rx_led"
-CONF_TX_LED = "tx_led"
 CONF_DISPLAY = "show_display"
 CONF_FORCE_MODE = "force_mode"
 CONF_VERTICAL_AIRFLOW = "vertical_airflow"
@@ -82,14 +79,6 @@ VERTICAL_SWING_DIRECTION_OPTIONS = {
     "DOWNSIDE": VerticalSwingDirection.DOWNSIDE,
 }
 
-HorizontalSwingDirection = tclac_ns.enum("HorizontalSwingDirection", True)
-HORIZONTAL_SWING_DIRECTION_OPTIONS = {
-    "LEFT_RIGHT": HorizontalSwingDirection.LEFT_RIGHT,
-    "LEFTSIDE": HorizontalSwingDirection.LEFTSIDE,
-    "CENTER": HorizontalSwingDirection.CENTER,
-    "RIGHTSIDE": HorizontalSwingDirection.RIGHTSIDE,
-}
-
 AirflowVerticalDirection = tclac_ns.enum("AirflowVerticalDirection", True)
 AIRFLOW_VERTICAL_DIRECTION_OPTIONS = {
     "LAST": AirflowVerticalDirection.LAST,
@@ -98,16 +87,6 @@ AIRFLOW_VERTICAL_DIRECTION_OPTIONS = {
     "CENTER": AirflowVerticalDirection.CENTER,
     "DOWN": AirflowVerticalDirection.DOWN,
     "MAX_DOWN": AirflowVerticalDirection.MAX_DOWN,
-}
-
-AirflowHorizontalDirection = tclac_ns.enum("AirflowHorizontalDirection", True)
-AIRFLOW_HORIZONTAL_DIRECTION_OPTIONS = {
-    "LAST": AirflowHorizontalDirection.LAST,
-    "MAX_LEFT": AirflowHorizontalDirection.MAX_LEFT,
-    "LEFT": AirflowHorizontalDirection.LEFT,
-    "CENTER": AirflowHorizontalDirection.CENTER,
-    "RIGHT": AirflowHorizontalDirection.RIGHT,
-    "MAX_RIGHT": AirflowHorizontalDirection.MAX_RIGHT,
 }
 
 # Проверка конфигурации интерфейса и принятие значений по умолчанию
@@ -149,10 +128,8 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_MODULE_DISPLAY, default=True): cv.boolean,
             cv.Optional(CONF_VERTICAL_AIRFLOW, default="CENTER"): cv.ensure_list(cv.enum(AIRFLOW_VERTICAL_DIRECTION_OPTIONS, upper=True)),
             cv.Optional(CONF_VERTICAL_SWING_MODE, default="UP_DOWN"): cv.ensure_list(cv.enum(VERTICAL_SWING_DIRECTION_OPTIONS, upper=True)),
-            cv.Optional(CONF_HORIZONTAL_AIRFLOW, default="CENTER"): cv.ensure_list(cv.enum(AIRFLOW_HORIZONTAL_DIRECTION_OPTIONS, upper=True)),
-            cv.Optional(CONF_HORIZONTAL_SWING_MODE, default="LEFT_RIGHT"): cv.ensure_list(cv.enum(HORIZONTAL_SWING_DIRECTION_OPTIONS, upper=True)),
             cv.Optional(CONF_SUPPORTED_PRESETS,default=["NONE","ECO","SLEEP",],): cv.ensure_list(cv.enum(SUPPORTED_CLIMATE_PRESETS_OPTIONS, upper=True)),
-            cv.Optional(CONF_SUPPORTED_SWING_MODES,default=["OFF","VERTICAL","HORIZONTAL","BOTH",],): cv.ensure_list(cv.enum(SUPPORTED_SWING_MODES_OPTIONS, upper=True)),
+            cv.Optional(CONF_SUPPORTED_SWING_MODES,default=["OFF","VERTICAL",],): cv.ensure_list(cv.enum(SUPPORTED_SWING_MODES_OPTIONS, upper=True)),
             cv.Optional(CONF_SUPPORTED_MODES,default=["OFF","AUTO","COOL","HEAT","DRY","FAN_ONLY",],): cv.ensure_list(cv.enum(SUPPORTED_CLIMATE_MODES_OPTIONS, upper=True)),
             cv.Optional(CONF_SUPPORTED_FAN_MODES,default=["AUTO","QUIET","LOW","MIDDLE","MEDIUM","HIGH","FOCUS","DIFFUSE",],): cv.ensure_list(cv.enum(SUPPORTED_FAN_MODES_OPTIONS, upper=True)),
         }
@@ -171,9 +148,7 @@ DisplayOffAction = tclac_ns.class_("DisplayOffAction", automation.Action)
 ModuleDisplayOnAction = tclac_ns.class_("ModuleDisplayOnAction", automation.Action)
 VerticalAirflowAction = tclac_ns.class_("VerticalAirflowAction", automation.Action)
 ModuleDisplayOffAction = tclac_ns.class_("ModuleDisplayOffAction", automation.Action)
-HorizontalAirflowAction = tclac_ns.class_("HorizontalAirflowAction", automation.Action)
 VerticalSwingDirectionAction = tclac_ns.class_("VerticalSwingDirectionAction", automation.Action)
-HorizontalSwingDirectionAction = tclac_ns.class_("HorizontalSwingDirectionAction", automation.Action)
 
 TCLAC_ACTION_BASE_SCHEMA = automation.maybe_simple_id({cv.GenerateID(CONF_ID): cv.use_id(tclacClimate),})
 
@@ -245,26 +220,6 @@ async def tclac_set_vertical_airflow_to_code(config, action_id, template_arg, ar
     cg.add(var.set_direction(template_))
     return var
 
-
-# Регистрация события установки горизонтальной фиксации заслонок
-@automation.register_action(
-    "climate.tclac.set_horizontal_airflow",
-    HorizontalAirflowAction,
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.use_id(tclacClimate),
-            cv.Required(CONF_HORIZONTAL_AIRFLOW): cv.templatable(cv.enum(AIRFLOW_HORIZONTAL_DIRECTION_OPTIONS, upper=True)),
-        }
-    ),
-)
-async def tclac_set_horizontal_airflow_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    template_ = await cg.templatable(config[CONF_HORIZONTAL_AIRFLOW], args, AirflowHorizontalDirection)
-    cg.add(var.set_direction(template_))
-    return var
-
-
 # Регистрация события установки вертикального качания шторки
 @automation.register_action(
     "climate.tclac.set_vertical_swing_direction",
@@ -282,26 +237,6 @@ async def tclac_set_vertical_swing_direction_to_code(config, action_id, template
     template_ = await cg.templatable(config[CONF_VERTICAL_SWING_MODE], args, VerticalSwingDirection)
     cg.add(var.set_swing_direction(template_))
     return var
-
-
-# Регистрация события установки горизонтального качания шторок
-@automation.register_action(
-    "climate.tclac.set_horizontal_swing_direction",
-    HorizontalSwingDirectionAction,
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.use_id(tclacClimate),
-            cv.Required(CONF_HORIZONTAL_SWING_MODE): cv.templatable(cv.enum(HORIZONTAL_SWING_DIRECTION_OPTIONS, upper=True)),
-        }
-    ),
-)
-async def tclac_set_horizontal_swing_direction_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    template_ = await cg.templatable(config[CONF_HORIZONTAL_SWING_MODE], args, HorizontalSwingDirection)
-    cg.add(var.set_swing_direction(template_))
-    return var
-
 
 # Добавление конфигурации в код
 def to_code(config):
@@ -326,12 +261,3 @@ def to_code(config):
         cg.add(var.set_supported_fan_modes(config[CONF_SUPPORTED_FAN_MODES]))
     if CONF_SUPPORTED_SWING_MODES in config:
         cg.add(var.set_supported_swing_modes(config[CONF_SUPPORTED_SWING_MODES]))
-
-    if CONF_TX_LED in config:
-        cg.add_define("CONF_TX_LED")
-        tx_led_pin = yield cg.gpio_pin_expression(config[CONF_TX_LED])
-        cg.add(var.set_tx_led_pin(tx_led_pin))
-    if CONF_RX_LED in config:
-        cg.add_define("CONF_RX_LED")
-        rx_led_pin = yield cg.gpio_pin_expression(config[CONF_RX_LED])
-        cg.add(var.set_rx_led_pin(rx_led_pin))
