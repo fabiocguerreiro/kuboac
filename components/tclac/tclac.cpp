c9fc298736db27f801d0d24586c13bfada7a8eb2
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2025-08-11 12:57:13 +0100

    Update tclac.cpp

--- components/tclac/tclac.cpp
+++ components/tclac/tclac.cpp
@@ -99,9 +99,6 @@ void tclacClimate::update() {
 void tclacClimate::readData() {
 	
 	current_temperature = float((( (dataRX[17] << 8) | dataRX[18] ) / 374 - 32)/1.8);
-	if (this->temperature_sensor_ != nullptr) {
-  this->temperature_sensor_->publish_state(this->current_temperature);
-}
 	target_temperature = (dataRX[FAN_SPEED_POS] & SET_TEMP_MASK) + 16;
 
 	//ESP_LOGD("TCL", "TEMP: %f ", current_temperature);
