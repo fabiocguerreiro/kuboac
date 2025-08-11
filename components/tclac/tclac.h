8fddbe1620df840f055090f702390f9062dd3d07
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2025-08-11 12:47:13 +0100

    Update tclac.h

--- components/tclac/tclac.h
+++ components/tclac/tclac.h
@@ -5,6 +5,7 @@
 #include "esphome/core/defines.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/climate/climate.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace tclac {
