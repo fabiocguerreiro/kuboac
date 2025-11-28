57a81d001f36bd56023c971095bca3c69f542adf
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2025-11-28 15:07:11 +0000

    Update automation.h

--- components/tclac/automation.h
+++ components/tclac/automation.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "esphome/core/automation.h"
 #include "tclac.h"
@@ -6,93 +6,103 @@
 namespace esphome {
 namespace tclac {
 
-template<typename... Ts> class VerticalAirflowAction : public Action<Ts...> {
+template<typename... Ts>
+class VerticalAirflowAction : public Action<Ts...> {
  public:
   VerticalAirflowAction(tclacClimate *parent) : parent_(parent) {}
   TEMPLATABLE_VALUE(AirflowVerticalDirection, direction)
-  void play(Ts... x) { this->parent_->set_vertical_airflow(this->direction_.value(x...)); }
+  void play(Ts... x) override { this->parent_->set_vertical_airflow(this->direction_.value(x...)); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class VerticalSwingDirectionAction : public Action<Ts...> {
+template<typename... Ts>
+class VerticalSwingDirectionAction : public Action<Ts...> {
  public:
   VerticalSwingDirectionAction(tclacClimate *parent) : parent_(parent) {}
   TEMPLATABLE_VALUE(VerticalSwingDirection, direction)
-  void play(Ts... x) { this->parent_->set_vertical_swing_direction(this->direction_.value(x...)); }
+  void play(Ts... x) override { this->parent_->set_vertical_swing_direction(this->direction_.value(x...)); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class DisplayOnAction : public Action<Ts...> {
+template<typename... Ts>
+class DisplayOnAction : public Action<Ts...> {
  public:
   DisplayOnAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_display_state(true); }
+  void play(Ts... x) override { this->parent_->set_display_state(true); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class DisplayOffAction : public Action<Ts...> {
+template<typename... Ts>
+class DisplayOffAction : public Action<Ts...> {
  public:
   DisplayOffAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_display_state(false); }
+  void play(Ts... x) override { this->parent_->set_display_state(false); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class BeeperOnAction : public Action<Ts...> {
+template<typename... Ts>
+class BeeperOnAction : public Action<Ts...> {
  public:
   BeeperOnAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_beeper_state(true); }
+  void play(Ts... x) override { this->parent_->set_beeper_state(true); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class BeeperOffAction : public Action<Ts...> {
+template<typename... Ts>
+class BeeperOffAction : public Action<Ts...> {
  public:
   BeeperOffAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_beeper_state(false); }
+  void play(Ts... x) override { this->parent_->set_beeper_state(false); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class ModuleDisplayOnAction : public Action<Ts...> {
+template<typename... Ts>
+class ModuleDisplayOnAction : public Action<Ts...> {
  public:
   ModuleDisplayOnAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_module_display_state(true); }
+  void play(Ts... x) override { this->parent_->set_module_display_state(true); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class ModuleDisplayOffAction : public Action<Ts...> {
+template<typename... Ts>
+class ModuleDisplayOffAction : public Action<Ts...> {
  public:
   ModuleDisplayOffAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_module_display_state(false); }
+  void play(Ts... x) override { this->parent_->set_module_display_state(false); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class ForceOnAction : public Action<Ts...> {
+template<typename... Ts>
+class ForceOnAction : public Action<Ts...> {
  public:
   ForceOnAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_force_mode_state(true); }
+  void play(Ts... x) override { this->parent_->set_force_mode_state(true); }
 
  protected:
   tclacClimate *parent_;
 };
 
-template<typename... Ts> class ForceOffAction : public Action<Ts...> {
+template<typename... Ts>
+class ForceOffAction : public Action<Ts...> {
  public:
   ForceOffAction(tclacClimate *parent) : parent_(parent) {}
-  void play(Ts... x) { this->parent_->set_force_mode_state(false); }
+  void play(Ts... x) override { this->parent_->set_force_mode_state(false); }
 
  protected:
   tclacClimate *parent_;
